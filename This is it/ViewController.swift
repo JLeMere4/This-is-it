62347d63833bef780e2af768439de56ae100ebd0
JLeMere4 <117326370+JLeMere4@users.noreply.github.com>
2022-11-15 09:08:11 -0700

    test 3

--- This is it/ViewController.swift
+++ This is it/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Hello")
+        print("I think Brandon did it")
     }
 
 
