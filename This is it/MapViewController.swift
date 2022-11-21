589e6498f02829f9912131048d1e76499dca7fe8
WynnLawrence <email@blahblah.com>
2022-11-21 09:19:48 -0700

    Basic Map Code Honolulu

--- This is it/MapViewController.swift
+++ This is it/MapViewController.swift
@@ -5,25 +5,52 @@
 //  Created by Jack on 11/16/22.
 //
 
+import CoreLocation
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
-
+class ViewController: UIViewController {
+    @IBOutlet private var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Set initial location in Honolulu
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        mapView.centerToLocation(initialLocation)
+        let oahuCenter = CLLocation(latitude: 21.4765, longitude: -157.9647)
+        let region = MKCoordinateRegion(
+          center: oahuCenter.coordinate,
+          latitudinalMeters: 50000,
+          longitudinalMeters: 60000)
+        mapView.setCameraBoundary(
+          MKMapView.CameraBoundary(coordinateRegion: region),
+          animated: true)
+        
+        let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
+        mapView.setCameraZoomRange(zoomRange, animated: true)
+
 
         // Do any additional setup after loading the view.
+        
+        //view.backgroundColor = .green
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
+
+private extension MKMapView {
+  func centerToLocation(
+    _ location: CLLocation,
+    regionRadius: CLLocationDistance = 1000
+  ) {
+    let coordinateRegion = MKCoordinateRegion(
+      center: location.coordinate,
+      latitudinalMeters: regionRadius,
+      longitudinalMeters: regionRadius)
+    setRegion(coordinateRegion, animated: true)
+  }
+}
+
+
+
+
--- This is it/ViewController.swift
+++ This is it/ViewController.swift
@@ -5,18 +5,35 @@
 //  Created by Jack on 11/14/22.
 //
 //https://developer.apple.com/documentation/mapkit/enabling_maps_capability_in_xcode
-
+/*
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class iewController: UIViewController {
+    @IBOutlet private var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Set initial location in Honolulu
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+
         // Do any additional setup after loading the view.
         
         //view.backgroundColor = .green
+    }
+    private extension MKMapView {
+      func centerToLocation(
+        _ location: CLLocation,
+        regionRadius: CLLocationDistance = 1000
+      ) {
+        let coordinateRegion = MKCoordinateRegion(
+          center: location.coordinate,
+          latitudinalMeters: regionRadius,
+          longitudinalMeters: regionRadius)
+        setRegion(coordinateRegion, animated: true)
+      }
     }
 
 
 }
 
+*/
